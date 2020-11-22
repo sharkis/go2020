2e5384f7c695f981aa05d6a02d14728a65b6578b
Albert Sharkis <sharkis@sharkis.org>
2020-11-21 22:00:11 -0700

    add shooting controls

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float Speed = 10.0f;
+    public GameObject shotPrefab;
+    Rigidbody2D rigidbody2d;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        lookDirection.Set(horizontal, 0);
+        lookDirection.Normalize();
+        Vector2 position = transform.position;
+        position.x = position.x + Speed * horizontal * Time.deltaTime;
+        transform.position = position;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Launch();
+        }
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(shotPrefab, rigidbody2d.position, Quaternion.Euler(0f,(lookDirection.x - 1)*90,0f));
+    }
+}
---
+++ Assets/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public void Start()
+    {
+        rb.velocity = transform.right * 20f;
+    }
+
+    void Update()
+    {
+        if (transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+}
